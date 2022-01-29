a073697a4cb214f0d5e57ec4116e5e030be2d1df
Aaron <anaugustchill@gmail.com>
2022-01-28 21:01:22 -0500

    got even digit number to work, working on odd

--- Projects/Palindrome/script.js
+++ Projects/Palindrome/script.js
@@ -1,3 +1,7 @@
+const reverseNum = num => {
+ let reversed = num.split("").reverse().join("");
+  return reversed;
+}
 const checkPalindrome = num => {
   console.log(`Original Number: ${num}`);
   let numString = num.toString();
@@ -11,9 +15,27 @@ const checkPalindrome = num => {
     console.log(firstHalf);
     let secondHalf = numString.slice(halfOfCharacters, numCharacters);
     console.log(secondHalf);
-    
+   let reversedSecondHalf = reverseNum(secondHalf);
+    console.log(reversedSecondHalf);
+    if(firstHalf == reversedSecondHalf){
+      return `${num} is a palindrome`;
+     
+    }else{
+      `${num} is not a palindrome`;
+    };
   } else if (numCharacters % 2 === 1){
-    
-    
+    let halfOfCharacters = (numCharacters -1) / 2;
+    console.log(`Half the Length Minus One of a Number with Odd Amount of Digits: ${halfOfCharacters}`);
+    let firstHalf = numString.slice(0,halfOfCharacters);
+    console.log(firstHalf);
+    let secondHalf = numString.slice(halfOfCharacters, numCharacters);
+    console.log(secondHalf);
+   let reversedSecondHalf = secondHalf.split("").reverse().join("");
+    console.log(reversedSecondHalf);
+    if(firstHalf == reversedSecondHalf){
+      return `${num} is a palindrome`
+    }else{
+      return `${num} is not a palindrome`
+    }
   }
 }
